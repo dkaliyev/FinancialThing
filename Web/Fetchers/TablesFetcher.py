a49ec4ecca2597dc97ff7cfb8fa2a717a91521ea
Daniyar <kaliyev.daniyar@gmail.com>
2015-07-29 20:30:08 +0100

    Added all

--- Web/Fetchers/TablesFetcher.py
+++ Web/Fetchers/TablesFetcher.py
@@ -1,1 +1,10 @@
 __author__ = 'Daniyar'
+
+class TablesFetcher:
+    def fetch(self, model):
+        new_list = []
+        for report in model['data']:
+            for year in report['data']:
+                for subtype in year['data']:
+                    for data in subtype['data']:
+            new_list.append({'subtype': subtypes})
